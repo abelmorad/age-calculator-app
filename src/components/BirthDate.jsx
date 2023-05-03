92a4f2a77cb65806762e06694221b1bf3cdb96a5
Studentabel <abelmorad97@gmail.com>
2023-05-03 21:22:15 +0800

    Update

--- src/components/BirthDate.jsx
+++ src/components/BirthDate.jsx
@@ -4,14 +4,17 @@ export default function BirthDate() {
             <div className="input-container">
                 <label className="label-style">DAY</label>
                 <input className="input" type="text" placeholder="DD" />
+                <p className="error-msg">Error</p>
             </div>
             <div className="input-container">
                 <label className="label-style">MONTH</label>
                 <input className="input" type="text"  placeholder="MM" />
+                <p className="error-msg">Error</p>
             </div>
             <div className="input-container">
                 <label className="label-style">YEAR</label>
                 <input className="input" type="text" placeholder="YYYY" />
+                <p className="error-msg">Error</p>
             </div>
         </section>
     )
